6a80c683cbd9afe3080b4a96d380f2f91ddfdd67
Ledenel <ledenelintelli@gmail.com>
2020-05-24 15:32:31 +0800

    extract get_hparams out

--- roberta_eval.py
+++ roberta_eval.py
@@ -27,79 +27,25 @@ class RobertaCodeQuerySoftmax(finetuning.CodeQuerySoftmax):
         _, embedding = self.model(*args, **kwargs)
         return embedding
 
-def get_hparams(snakemake):
-    seed = int(snakemake.params.seed)
-
-    fast = snakemake.params.fast
-#     pct = 0.05 if fast else 1.0
-    test_batch_size = 1000#int(1000 * pct)
-
-    fast_str = ("_fast" if fast else "")
-    run_name = f"roberta_base_on_{snakemake.wildcards.lang}_{snakemake.wildcards.extra}{fast_str}"
-    wandb_logger = pl.loggers.WandbLogger( 
-        name=run_name,
-        project="csnet-roberta",
-    )
-    config = AutoConfig.from_pretrained("huggingface/CodeBERTa-small-v1", resume_download=True)
-    ckpt = pl.callbacks.ModelCheckpoint(filepath='saved_module/'+run_name+'/{epoch}-mrr{val_loss:.4f}', mode="max")
-    # print(f"{snakemake.config}")
-    if "gpu_ids" in snakemake.config:
-        gpu_ids = ast.literal_eval(str(snakemake.config["gpu_ids"]).strip())
-    else:
-        gpu_ids = 0
-
-    hparams = {
-        "dev_mode": fast,
-        "seed": seed,
-        "roberta_config": config.to_dict(),
-        "datapath": snakemake.input,
-        "test_batch": 1000,
-        "train_batch": 64,
-        "train_max_len": 200,
-    }
+def get_hparams(snakemake, hparams_override=None):
+    hparams, _ = pre_configure(hparams_override, snakemake)
     return hparams
 
 def main(snakemake, model=None, hparams_override=None):
-    seed = int(snakemake.params.seed)
+    hparams, run_name = pre_configure(hparams_override, snakemake)
 
-    fast = "dev_fast" in snakemake.config and int(snakemake.config["dev_fast"])
-#     pct = 0.05 if fast else 1.0
-    test_batch_size = 1000#int(1000 * pct)
-
-    fast_str = ("_fast" if fast else "")
-    run_name = f"roberta_base_on_{snakemake.wildcards.lang}_{snakemake.wildcards.extra}{fast_str}"
-    wandb_logger = pl.loggers.WandbLogger( 
+    wandb_logger = pl.loggers.WandbLogger(
         name=run_name,
         project="csnet-roberta",
     )
-    config = AutoConfig.from_pretrained("huggingface/CodeBERTa-small-v1", resume_download=True)
-    ckpt = pl.callbacks.ModelCheckpoint(filepath='saved_module/'+run_name+'/{epoch}-mrr{val_loss:.4f}', mode="max")
-    # print(f"{snakemake.config}")
-    if "gpu_ids" in snakemake.config:
-        gpu_ids = ast.literal_eval(str(snakemake.config["gpu_ids"]).strip())
-    else:
-        gpu_ids = 0
 
-    hparams = {
-        "dev_mode": fast,
-        "seed": seed,
-        "roberta_config": config.to_dict(),
-        "datapath": snakemake.input,
-        "test_batch": 1000,
-        "train_batch": 64,
-        "train_max_len": 200,
-        "method": "roberta-pretrain",
-        "max_epochs": 5,
-    }
+    ckpt = pl.callbacks.ModelCheckpoint(filepath='saved_module/' + run_name + '/{epoch}-mrr{val_loss:.4f}', mode="max")
 
-    if hparams_override is not None:
-        hparams.update(hparams_override)
-    
-    np.random.seed(seed)
-    torch.manual_seed(seed)
+    np.random.seed(hparams["seed"])
+    torch.manual_seed(hparams["seed"])
     trainer = pl.Trainer(
-        gpus=gpu_ids,
-        fast_dev_run=fast,
+        gpus=hparams["gpu_ids"],
+        fast_dev_run=hparams["dev_mode"],
         logger=wandb_logger,
         checkpoint_callbacks=[ckpt],
         max_epochs=hparams["max_epochs"]
@@ -110,6 +56,37 @@ def main(snakemake, model=None, hparams_override=None):
     trainer.fit(model)
     # TODO: verify that lighting will pick best model evaluated in validation.
     trainer.test(model)
+
+
+def pre_configure(hparams_override, snakemake):
+    seed = int(snakemake.params.seed)
+    fast = "dev_fast" in snakemake.config and int(snakemake.config["dev_fast"])
+    #     pct = 0.05 if fast else 1.0
+    test_batch_size = 1000  # int(1000 * pct)
+    fast_str = ("_fast" if fast else "")
+    run_name = f"roberta_base_on_{snakemake.wildcards.lang}_{snakemake.wildcards.extra}{fast_str}"
+    config = AutoConfig.from_pretrained("huggingface/CodeBERTa-small-v1", resume_download=True)
+    # print(f"{snakemake.config}")
+    if "gpu_ids" in snakemake.config:
+        gpu_ids = ast.literal_eval(str(snakemake.config["gpu_ids"]).strip())
+    else:
+        gpu_ids = 0
+    hparams = {
+        "dev_mode": fast,
+        "seed": seed,
+        "roberta_config": config.to_dict(),
+        "datapath": snakemake.input,
+        "test_batch": 1000,
+        "train_batch": 64,
+        "train_max_len": 200,
+        "method": "roberta-pretrain",
+        "max_epochs": 5,
+        "gpu_ids": gpu_ids,
+    }
+    if hparams_override is not None:
+        hparams.update(hparams_override)
+    return hparams, run_name
+
 
 if __name__ == "__main__":
     main(snakemake)
