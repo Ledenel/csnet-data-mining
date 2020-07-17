c22ad346934f1c220527f5b0186f6864876bdbfe
Ledenel <ledenelintelli@gmail.com>
2020-07-18 00:38:52 +0800

    add model attn export demo.

--- explore_model.py
+++ explore_model.py
@@ -3,6 +3,7 @@ from transformers import AutoTokenizer, AutoModel
 from roberta_ast_label_pretrain import FinetuningRoberta, RobertaPretrain
 from roberta_mask_pretrain import RobertaMaskPretrain, FinetuningMaskRoberta
 import pandas as pd
+import xarray as xa
 
 if __name__ == '__main__':
     test_data = pd.read_pickle("data_cache/java_train_0.pkl")
@@ -15,5 +16,9 @@ if __name__ == '__main__':
         "huggingface/CodeBERTa-small-v1", resume_download=True)
 
     encoded = tokenizer.encode_plus(test_data.docstring.array[0], test_data.code.array[0], return_tensors="pt")
-    output, attentions = model(**encoded, output_attentions=True)
-    print(output)
+    hiddens, first_output, attentions = model(**encoded, output_attentions=True)
+
+    attn_array = xa.DataArray(data=[attn.detach().numpy() for attn in attentions],
+                              dims=["layer", "batch", "head", "query_pos", "key_pos"])
+
+    attn_array.to_dataframe("attn").to_csv("test_attn.csv")
